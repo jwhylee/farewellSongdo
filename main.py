55706884d7cd0b17b105e6ded3f76adc007cc14c
Jaeyeol Lee <jy_323@naver.com>
2026-04-02 23:42:02 +0900

    add char_stat, env_stat, location

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# 주인공 상태 변수 : 딕셔너리 - 배고픔정도
+char_status = {
+    "levelHunger": "배고픔",
+}
+
+# 환경 상태 변수 : 딕셔너리 - 시간
+env_status ={
+    "time": 1100
+}
+
+# 위치 변수 : 리스트 - [x,y]
+location = "연대앞 버스정류장" 
+location_idx = [0,0]
+
+
 def main():
     print("Hello from farewellsongdo!")
 
